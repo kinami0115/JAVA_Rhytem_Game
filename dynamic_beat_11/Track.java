0c64d53e88ece9e8539f5330459addee6a02dc89
kinam <52563597+kinami0115@users.noreply.github.com>
2019-08-04 02:17:41 +0900

    Add files via upload

---
+++ dynamic_beat_11/Track.java
@@ -0,0 +1,50 @@
+package dynamic_beat_11;
+
+public class Track {
+	
+	private String titleImage; //제목 부분 이미지
+	private String stratImage; // 게임 선택 창 표지 이미지
+	private String gameImage; // 해당 곡을 실행 했을 때 표지
+	private String startMusic; // 게임 선택 창 음악
+	private String gameMusic; // 해당 곡을 실행 했을 때 음악
+	public String getTitleImage() {
+		return titleImage;
+	}
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+	public String getStratImage() {
+		return stratImage;
+	}
+	public void setStratImage(String stratImage) {
+		this.stratImage = stratImage;
+	}
+	public String getGameImage() {
+		return gameImage;
+	}
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+	public String getStartMusic() {
+		return startMusic;
+	}
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+	public String getGameMusic() {
+		return gameMusic;
+	}
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+	public Track(String titleImage, String stratImage, String gameImage, String startMusic, String gameMusic) {
+		super();
+		this.titleImage = titleImage;
+		this.stratImage = stratImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+	}
+	
+	
+}
